2c82dccd606b0efc2da74715d6a331c0bf5362a4
hasnabila321 <hasnabila17@gmail.com>
2023-05-06 23:05:36 +0700

    menambahkan layoutan admin

---
+++ app/src/main/java/com/example/troisapp/InterfaceStaff.java
@@ -0,0 +1,14 @@
+package com.example.troisapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InterfaceStaff extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interface_staff);
+    }
+}
